03c108c5caf89887170cfba8c14bb3b0417b3de6
juanesbrice11 <juanesbrice977@gmail.com>
2024-12-31 20:57:20 -0500

    Añadiendo vista para busqueda de clientes

--- src/app/historiaclinica/page.tsx
+++ src/app/historiaclinica/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import NavbarMenu from '@/components/Navbar'
 import HistoriaClinica from '@/components/HistoriaClinica'
 import Footer from '@/components/Footer'
 
 export default function page() {
   return (
     <div className="min-h-screen flex flex-col" >
-      <NavbarMenu />
       <HistoriaClinica />
-      <Footer />
     </div>
 
   )
